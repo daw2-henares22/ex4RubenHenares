e97f427e9d18fb8aa05082dae35c61475806eb9f
daw2-henares22 <91276223+daw2-henares22@users.noreply.github.com>
2024-05-31 16:41:02 +0000

    historia6

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,11 +1,20 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import db from '../bd.js';
 
 export const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-  const [ticketsPendientes, setTicketsPendientes] = useState(db.ticketsPendientes);
-  const [ticketsResueltos, setTicketsResueltos] = useState(db.ticketsResueltos);
+  const [ticketsPendientes, setTicketsPendientes] = useState([]);
+  const [ticketsResueltos, setTicketsResueltos] = useState([]);
+
+  useEffect(() => {
+    
+    const pendientesOrdenados = db.ticketsPendientes.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+    setTicketsPendientes(pendientesOrdenados);
+
+    const resueltosOrdenados = db.ticketsResueltos.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+    setTicketsResueltos(resueltosOrdenados);
+  }, []);
 
   return (
     <GlobalContext.Provider value={{ ticketsPendientes, setTicketsPendientes, ticketsResueltos, setTicketsResueltos }}>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,17 +3,17 @@ export function Login(){
         <>
         <div className="container pt-5">
         <h1 className="w-100 text-center">Registro</h1>
-        <form>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" className="form-control" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" className="form-control" />
-      </div>
-      <button type="submit" className="btn btn-primary">Login</button>
-    </form>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" className="form-control" />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
         </div>
         </>
     )
